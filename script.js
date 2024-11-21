500156269566cd0f0beaece82841de6fda82d028
ligaliomar <ligaliomar64@gmail.com>
2024-11-21 21:49:43 +0100

    Mes modifs

--- script.js
+++ script.js
@@ -16,40 +16,47 @@ let countdown;
 
 const quizArray=[
     {
+        id:1,
         question: "Quelle est la spécialité de Pascal Vicat-Blanc ?",
         options: ["Réseaux informatiques", "Cybersécurité", "Intelligence Artificielle", "Systèmes distribués"],
-        answer: "Systèmes distribués"
+        correct: "Systèmes distribués",
     },
     {
+        id:2,
         question: "Quel est le domaine de recherche principal de Pascal Vicat-Blanc ?",
         options: ["Cloud Computing", "Data Mining", "Sécurité réseau", "Virtualisation"],
-        answer: "Virtualisation"
+        correct: "Virtualisation",
     },
     {
+        id:3,
         question: "En quelle année Pascal Vicat-Blanc a-t-elle fondé la société Lyatiss ?",
         options: ["2010", "2008", "2012", "2015"],
-        answer: "2010"
+        orrect: "2010"
     },
     {
+        id:4,
         question: "Quelle entreprise a été fondée par Pascal Vicat-Blanc ?",
         options: ["Lyatiss", "Oracle", "Amazon Web Services", "Cisco"],
-  !      answer: "Lyatiss"
+        correct: "Lyatiss",
     },
     {
+        id:5,
         question: "Quel est l'un des principaux objectifs de ses recherches ?",
         options: ["Optimisation des flux de données dans le cloud", "Développement d'algorithmes IA", "Cyberattaques", "Gestion de bases de données"],
-        answer: "Optimisation des flux de données dans le cloud"
+        correct: "Optimisation des flux de données dans le cloud",
     },
     {
+        id:6,
         question: "Pascal Vicat-Blanc est une experte dans quel domaine technologique émergent ?",
         options: ["Edge Computing", "Blockchain", "Virtualisation", "Machine Learning"],
-        answer: "Virtualisation"
+        correct: "Virtualisation",
     },
     {
+        id:7,
         question: "Quel poste Pascal Vicat-Blanc a-t-elle occupé avant de fonder Lyatiss ?",
         options: ["Chercheuse CNRS", "Professeur à l'Université", "Directrice technique chez Cisco", "Consultante chez IBM"],
-        answer: "Chercheuse CNRS"
-    }
+        correct: "Chercheuse CNRS",
+    },
 ];
 
 restart.addEventlistener("click",()=>{
@@ -58,7 +65,7 @@ restart.addEventlistener("click",()=>{
     scoreContainer.classList.add("hide");
 });
 
-nextBtn.addEventlistener("click",(displayNext={} =>{
+nextBtn.addEventlistener("click",(displayNext=() =>{
     questionCount  += 1;
 
     if(questionCount == quizArray.length){
@@ -101,7 +108,7 @@ const quizDisplay=(questionCount)=>{
 };
 
 function quizCreater(){
-    quizArray.sort(()=>Math.random(-0.5);
+    quizArray.sort(()=>Math.random(-0.5));
 
     for (let i of quizArray){
         i.options.sort(()=>Math.random(-0.5));
@@ -116,13 +123,66 @@ function quizCreater(){
     question_DIV.innerHTML=i.question;
     div.appendChild(question_DIV);
 
-    div.innerHTML bn +=
+    div.innerHTML  +=`
     <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
     <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
     <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
 
-    <button class="option-div" onclick="checker(this)">${i.options[3]}</button>;
-
-     
+    <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+    `;
+    quizContainer.appendChild(div);
+    
 }
+}
+
+function checker(userOption){
+    let userSolution=userOption.innerText;
+    let question=document.getElementsByClassName
+    ("container-mid")[questionCount];
+    let options=question.querySelectorAll(".options-div");
+    
+    if(userSolution===quizArray[questionCount].
+    correct){
+        userOption.classList.add("correct");
+        scoreCount++;
+
+    }else{
+        userOption.classList.add("incorrect");
+        
+        options.forEach((element)=>{
+            if(element.innerText=quizArray[questionCount].
+                correct){
+                    element.classList.add("correct");
+                }
+        })
+    }
+    clearInterval(countdown);
+    options.forEach((element)=>{
+        element.disabled=true;
+    });
+
+}
+
+function initial(){
+    quizContainer.innerHTML="";
+    questionCount=0;
+    scoreCount=0;
+    scoreCount=0;
+    count=11;
+    clearInterval(countdown);
+    timerDisplay();
+    quizCreater();
+    quizDisplay(questionCount);
+}
+
+startButton.addEventListener("click",()=>{
+    startScreen.classList.add("hide");
+    displayContainer.classList.remove("hide");
+    initial();
+
+});
+
+window.onload=()=>{
+    startScreen.classList.remove("hide");
+    displayContainer.classList.add("hide");
 }
